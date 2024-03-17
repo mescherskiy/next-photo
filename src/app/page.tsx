714f4e304ca1ba794c4f3a8f2c799d898d65eadf
Alexander <mescherskiy.alexandr@gmail.com>
2024-03-17 22:31:25 +0100

    init commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,106 @@
-import Image from "next/image";
+"use client"
+
+import React, { useState } from 'react';
 
 export default function Home() {
+
+  type StateProps = {
+    brightness: number,
+    contrast: number,
+    grayscale: number,
+    hueRotate: number,
+    invert: number,
+    saturate: number,
+    sepia: number
+  }
+
+  const initialState: StateProps = {
+    brightness: 100,
+    contrast: 100,
+    grayscale: 0,
+    hueRotate: 0,
+    invert: 0,
+    saturate: 100,
+    sepia: 0
+  }
+
+  const [imageProps, setImageProps] = useState(initialState)
+  const [imageSrc, setImageSrc] = useState("")
+
+  // const [brightness, setBrightness] = useState(100)
+  // const [contrast, setContrast] = useState(100)
+  // const [grayscale, setGrayscale] = useState(0)
+  // const [hueRotate, setHueRotate] = useState(0)
+  // const [invert, setInvert] = useState(0)
+  // const [saturate, setSaturate] = useState(100)
+  // const [sepia, setSepia] = useState(0)
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImageSrc(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className='flex justify-between align-baseline'>
+        <div className='w-4/12'>
+          <h2>Original Image</h2>
+          {imageSrc && <img className='photo' src={imageSrc} alt="Original" />}
+          <input type="file" accept="image/*" onChange={handleImageChange} />
+        </div>
+        <div className='filters flex flex-col w-3/12'>
+          <label htmlFor="brightness">Brightness: <span>{imageProps.brightness}%</span></label>
+          <input type="range" name="brightness" min={0} max={200} value={imageProps.brightness}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setImageProps(props => ({...props, brightness: Number(e.target.value)}))} />
+          {/* <br /> */}
+          <label htmlFor="contrast">Contrast: <span>{imageProps.contrast}%</span></label>
+          <input type="range" name="contrast" min={0} max={200} value={imageProps.contrast} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setImageProps(props => ({...props, contrast: Number(e.target.value)}))} />
+          {/* <br /> */}
+          <label htmlFor="grayscale">Grayscale: <span>{imageProps.grayscale}%</span></label>
+          <input type="range" name="grayscale" min={0} max={100} value={imageProps.grayscale} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setImageProps(props => ({...props, grayscale: Number(e.target.value)}))} />
+          {/* <br /> */}
+          <label htmlFor="hue-rotate">Hue rotate: <span>{imageProps.hueRotate}&deg;</span></label>
+          <input type="range" name="hue-rotate" min={0} max={360} value={imageProps.hueRotate} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setImageProps(props => ({...props, hueRotate: Number(e.target.value)}))} />
+          {/* <br /> */}
+          <label htmlFor="invert">Invert: <span>{imageProps.invert}%</span></label>
+          <input type="range" name="invert" min={0} max={100} value={imageProps.invert} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setImageProps(props => ({...props, invert: Number(e.target.value)}))} />
+          {/* <br /> */}
+          <label htmlFor="saturate">Saturate: <span>{imageProps.saturate}%</span></label>
+          <input type="range" name="saturate" min={0} max={200} value={imageProps.saturate} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setImageProps(props => ({...props, saturate: Number(e.target.value)}))} />
+          {/* <br /> */}
+          <label htmlFor="sepia">Sepia: <span>{imageProps.sepia}%</span></label>
+          <input type="range" name="sepia" min={0} max={100} value={imageProps.sepia} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setImageProps(props => ({...props, sepia: Number(e.target.value)}))} />
+          <button onClick={() => {
+            setImageProps(props => ({
+              ...props,
+              brightness: 120,
+              contrast: 130,
+              hueRotate: 3,
+              saturate: 118,
+            }))
+          }}>Optimize</button>
+          <button onClick={() => setImageProps(initialState)}>Reset</button>
+        </div>
+        <div className='w-4/12'>
+          <h2>Processed Image</h2>
+          {imageSrc && 
+          <img className={`photo`}
+            src={imageSrc}
+            alt="Filtered"
+            style={{
+              filter:
+                `brightness(${imageProps.brightness}%) contrast(${imageProps.contrast}%) grayscale(${imageProps.grayscale}%) hue-rotate(${imageProps.hueRotate}deg) invert(${imageProps.invert}%) saturate(${imageProps.saturate}%) sepia(${imageProps.sepia}%)`
+            }} /> }
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+
